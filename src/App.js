8b48b2566ab6ef087ac3dea1c2e9c3994738a62d
Andres Arevalo <andresfelipear@gmail.com>
2021-11-19 04:48:10 -0800

    using enviroment variables in netlify

--- src/App.js
+++ src/App.js
@@ -50,7 +50,8 @@ function App() {
   }
 
   const getCityWeather = (city) => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_WEATHER_API}`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${REACT_APP_WEATHER_API}`
+    // const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=/.netlify/functions/todo`
 
 
     fetch(url).then(res => res.json()).then(
